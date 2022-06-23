a4117aea510a21cc873505eee806eae99d79f5ce
Kostya Rybak <62746770+K-Rybak@users.noreply.github.com>
2022-06-23 17:13:29 +0600

    Change name of property

--- InternetShop/Models/Category.cs
+++ InternetShop/Models/Category.cs
@@ -11,6 +11,6 @@ namespace InternetShop.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public int DisplayName { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
