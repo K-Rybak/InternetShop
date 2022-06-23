6d17a5f2912de2903de988bd4dc094ea8e138568
Kostya Rybak <62746770+K-Rybak@users.noreply.github.com>
2022-06-23 19:19:54 +0600

    Added display of categories

--- InternetShop/Controllers/CategoryController.cs
+++ InternetShop/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InternetShop.Data;
+using InternetShop.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,16 @@ namespace InternetShop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> categoryList = _db.Category;
+            return View(categoryList);
         }
     }
 }
